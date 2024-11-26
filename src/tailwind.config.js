c74ca929fd63569a3ee62137c33b590ea00f715f
23523082 <ettabego@gmail.com>
2024-11-26 12:25:08 +0700

    TAILWIND CSS COMPLETE

---
+++ src/tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    content: ["./public/*.{html,js}"], // Adjust this path based on where your HTML files are located
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
