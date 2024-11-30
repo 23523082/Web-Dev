6dca0e5ef178ebfc41890f69a475f6b983cb2978
Ravfael <ravfaelnovitohandoyo419@gmail.com>
2024-11-30 11:15:54 +0700

    hapus tailwind

--- src/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    content: ["./public/*.{html,js}"], // Adjust this path based on where your HTML files are located
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
-  
